13653cda948afcb4d46089668132122aa8a4a299
Preetham <23aiml098@bnmit.in>
2025-04-25 09:07:09 +0530

    Update facto.py

--- facto.py
+++ facto.py
@@ -1,7 +1,8 @@
-def fact(int n):
-    if n==1 or n==0:
-        return 1
-    else:
-        return n*fact(n-1)
-a=int(input("enter a number: "))
-print(fact(a))
+def facto(n):
+  if n==1 or n==0:
+    return 1
+  else:
+    return facto(n-1)*n
+a=int(input("Enter the number: "))
+b=facto(a)
+print("Factorial of ",a,": ",b)
